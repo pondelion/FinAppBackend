b482e9d807bd3931c0688ddf4b9e03b64f011494
pondelion <programming.deve@gmail.com>
2019-12-29 21:34:04 +0900

    update

--- fin_app/storage/base_storage.py
+++ fin_app/storage/base_storage.py
@@ -7,3 +7,13 @@ class BaseStorage(metaclass=ABCMeta):
     @abstractmethod
     def upload_file():
         raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def download_file():
+        raise NotImplementedError
+
+    @staticmethod
+    @abstractmethod
+    def get_filelist():
+        raise NotImplementedError
