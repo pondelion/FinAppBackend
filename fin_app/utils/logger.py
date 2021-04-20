dee2e87467a8d0b784fa32bfa7770e98b6fed981
pondelion <programming.deve@gmail.com>
2021-04-21 02:46:59 +0900

    update

--- fin_app/utils/logger.py
+++ fin_app/utils/logger.py
@@ -8,12 +8,12 @@ import yaml
 formatter = '%(levelname)s : %(asctime)s : %(message)s'
 logging.basicConfig(format=formatter)
 
-DEFAULT_DEV_FILEPATH = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
-    '..',
-    'config/dev.yml'
-)
-dev_conf = yaml.safe_load(open(DEFAULT_DEV_FILEPATH))
+# DEFAULT_DEV_FILEPATH = os.path.join(
+#     os.path.dirname(os.path.abspath(__file__)),
+#     '..',
+#     'config/dev.yml'
+# )
+# dev_conf = yaml.safe_load(open(DEFAULT_DEV_FILEPATH))
 filename = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     '..', '..',
@@ -27,35 +27,30 @@ file_handler.setFormatter(logging.Formatter(formatter))
 
 
 class Logger:
+    logger = logging.getLogger('fin_app')
+    if 'LAMBDA' not in os.environ:
+        logger.addHandler(file_handler)
 
     @staticmethod
     def d(tag: str, message: str):
         """debug log"""
-        logger = logging.getLogger('fin_app')
-        logger.setLevel(logging.DEBUG)
-        logger.addHandler(file_handler)
-        logger.debug('[%s] %s', tag, message)
+        Logger.logger.setLevel(logging.DEBUG)
+        Logger.logger.debug('[%s] %s', tag, message)
 
     @staticmethod
     def i(tag: str, message: str):
         """infomation log"""
-        logger = logging.getLogger('fin_app')
-        logger.setLevel(logging.INFO)
-        logger.addHandler(file_handler)
-        logger.info('[%s] %s', tag, message)
+        Logger.logger.setLevel(logging.INFO)
+        Logger.logger.info('[%s] %s', tag, message)
 
     @staticmethod
     def e(tag: str, message: str):
         """error log"""
-        logger = logging.getLogger('fin_app')
-        logger.setLevel(logging.ERROR)
-        logger.addHandler(file_handler)
-        logger.error('[%s] %s', tag, message)
+        Logger.logger.setLevel(logging.ERROR)
+        Logger.logger.error('[%s] %s', tag, message)
 
     @staticmethod
     def w(tag: str, message: str):
         """warning log"""
-        logger = logging.getLogger('fin_app')
-        logger.setLevel(logging.WARNING)
-        logger.addHandler(file_handler)
-        logger.warn('[%s] %s', tag, message)
+        Logger.logger.setLevel(logging.WARNING)
+        Logger.logger.warn('[%s] %s', tag, message)
